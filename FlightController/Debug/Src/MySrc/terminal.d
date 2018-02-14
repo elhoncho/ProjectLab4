0f742f95702731f3ab658331dbeaf1fb2f6685b2
elhoncho <elhoncho>
2018-02-13 19:50:24 -0600

    Updating .gitignore

--- FlightController/Debug/Src/MySrc/terminal.d
+++
@@ -1,7 +0,0 @@
-Src/MySrc/terminal.o: ../Src/MySrc/terminal.c \
- C:/Users/owner/Downloads/ProjectLab4-master/ProjectLab4-master/FlightController/Inc/MyInc\terminal.h \
- C:/Users/owner/Downloads/ProjectLab4-master/ProjectLab4-master/FlightController/Inc/MyInc\terminalHAL.h
-
-C:/Users/owner/Downloads/ProjectLab4-master/ProjectLab4-master/FlightController/Inc/MyInc\terminal.h:
-
-C:/Users/owner/Downloads/ProjectLab4-master/ProjectLab4-master/FlightController/Inc/MyInc\terminalHAL.h:
