fd0125bb1620c7aff6c9bffc05fc5473fb05ca2a
elhoncho <elhoncho>
2018-02-13 19:27:53 -0600

    Re arranging

--- FlightController/Debug/Src/MySrc/terminal.d
+++
@@ -1,7 +0,0 @@
-Src/MySrc/terminal.o: ../Src/MySrc/terminal.c \
- C:/Users/owner/Desktop/Workspace/FlightController/Inc/MyInc\terminal.h \
- C:/Users/owner/Desktop/Workspace/FlightController/Inc/MyInc\terminalHAL.h
-
-C:/Users/owner/Desktop/Workspace/FlightController/Inc/MyInc\terminal.h:
-
-C:/Users/owner/Desktop/Workspace/FlightController/Inc/MyInc\terminalHAL.h:
